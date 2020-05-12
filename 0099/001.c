d121b7f9b41938debb6a963a77af5e73d0456f8f
Hedrav Bands <hr19ut@brocku.ca>
2020-05-11 22:30:58 -0400

    brute force

--- 0099/001.c
+++ 0099/001.c
@@ -1,1 +1,15 @@
-teste for pushing
+#include <stdio.h>
+
+int main(void)
+{
+  int s3 = 0, s5 = 0, s15 = 0;
+  int i;
+  for (i = 0; i < 1000; i++) {
+    if (i % 3 == 0) { s3 += i;}
+    if (i % 5 == 0) { s5 += i;}
+    if (i % 15 == 0) {s15 += i;}
+  }
+  printf("%d\n", s3 + s5 - s15);
+
+  return 0;
+}
