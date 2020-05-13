f89ea2289f89766fed3ae69abd963414cc2c16c7
Hedrav Bands <hr19ut@brocku.ca>
2020-05-12 23:39:06 -0400

    using unsigned int

--- 0099/002.c
+++ 0099/002.c
@@ -1,1 +1,15 @@
-wow  clang
+#include <stdio.h>
+
+int main(void)
+{
+  unsigned int a1 = 1, a2 = 1, a3 = 2, sum = 0;
+
+  while (a3 < 4000000) {
+    a3 = a1 + a2;
+    sum += a3 * !(a3%2);
+    a1 = a2;
+    a2 = a3;
+  }
+  printf("%u\n", sum);
+  return 0;
+}
