3bfba756bda9991b61c5e5e298c2e078851ab992
Hedrav Bands <hr19ut@brocku.ca>
2020-05-15 23:47:25 -0400

    better format

--- 0099/002.c
+++ 0099/002.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-  int a1 = 1, a2 = 1, a3 = 2, sum = 0;
+  int a1 = 1, a2 = 1, sum = 0;
 
   while (a3 < 4000000) {
     a3 = a1 + a2;
     sum += a3 * !(a3%2);
     a1 = a2;
     a2 = a3;
-  }
+} //end while
   printf("%u\n", sum);
   return 0;
 }
