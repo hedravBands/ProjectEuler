36e161c17b8bd8da6030d2a87aa55f1050d6cf58
Hedrav Bands <hr19ut@brocku.ca>
2020-05-17 23:50:12 -0400

    for loop

---
+++ 0099/006.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  unsigned s1 = 0, s2 = 0, i; //declaring
+  for (i = 1; i <= 100; i++) {
+    s1 += i*i;
+    s2 += i;
+    } //end for
+  printf("%u\n", s2*s2 - s1);
+  return 0;
+} // end main
