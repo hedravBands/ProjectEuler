40e419e6bd1b4006725342fcbd3212c8716e902d
Hedrav Bands <60276490+hedravBands@users.noreply.github.com>
2020-05-22 23:01:36 -0400

    Update 001.c
    Formatting

--- 0099/001.c
+++ 0099/001.c
@@ -5,9 +5,12 @@ int main(void)
   int s3 = 0, s5 = 0, s15 = 0;
   int i;
   for (i = 0; i < 1000; i++) {
-    if (i % 3 == 0) { s3 += i;}
-    if (i % 5 == 0) { s5 += i;}
-    if (i % 15 == 0) {s15 += i;}
+    if (i % 3 == 0) { 
+       s3 += i;}
+    if (i % 5 == 0) { 
+       s5 += i;}
+    if (i % 15 == 0) {
+       s15 += i;}
   }
   printf("%d\n", s3 + s5 - s15);
 
